76af8983ce9ce0cabc1b6a81885e5fbf191bf193
Victor <162641571+Fantizzv@users.noreply.github.com>
2026-03-30 21:48:25 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,21 @@
+/**
+     * ESPAÇO PARA O DESAFIO
+     * Instruções: 
+     * 1. Capture os valores dos inputs (simulando o 'leia')
+     * 2. Converta para números Reais (Float)
+     * 3. Calcule a média aplicando a regra de precedência correta
+     * 4. Exiba no elemento #output (simulando o 'escreva')
+     */
+    function resolverDesafio() {
+        const outputDiv = document.getElementById('output')
+        let n1 = parseFloat(document.getElementById('n1').value)
+        let n2 = parseFloat(document.getElementById('n2').value)
+        let n3 = parseFloat(document.getElementById('n3').value)
+        let n4 = parseFloat(document.getElementById('n4').value)
+
+        let resolverDesafio = ((n1 + n2 + n3 + n4) /4)
+
+        outputDiv.innerHTML = 'A média é: ' + resolverDesafio
+        // let n1 = parseFloat(document.getElementById('n1').value);
+
+    }
